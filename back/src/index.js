eeaab0342896649c30a5bb42fd434bc353757ee9
Ramon Gonzalez <ramongonzalez101@gmail.com>
2025-06-15 23:45:54 -0400

    creacion del servidor

--- back/src/index.js
+++ back/src/index.js
@@ -1,3 +1,17 @@
 import express from 'express'
-import 
+import { pgdb } from './config.js'
+import morgan  from 'morgan'
+import cors from 'cors'
+
+
+const app = express ();
+
+app.use (morgan('dev'))
+app.use(cors());
+app.use (express.json())
+app.use (express.urlencoded({extended: false}))
+
+
+app.listen (pgdb.PORT)
+console.log ('conectado en el puerto', pgdb.PORT)
 
